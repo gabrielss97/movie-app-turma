ea3c419fc49347ad15a2245f5a8b65d4cf50754c
gabrielss97 <gabriel.ssalvador97@gmail.com>
2022-07-07 22:04:03 -0300

    façam o css

--- script.js
+++ script.js
@@ -17,14 +17,55 @@ async function getMovies(url) {
 }
 
 function createCards(movies) {
-  movies.forEach(movie => {
-    const movieCardEl = document.createElement("div")
-    movieCardEl.classList.add("movie")
+  mainEl.innerHTML = "";
+  movies.forEach((movie) => {
+    const movieCardEl = document.createElement("div");
+    movieCardEl.classList.add("movie");
 
     movieCardEl.innerHTML = `
-    <img src="${IMGPATH + movie.poster_path}" alt="${movie.title}"/>
-    <h1>${movie.title}</h1>
-    `
-    mainEl.appendChild(movieCardEl)
+    <img
+    src="${IMGPATH + movie.poster_path}"
+    alt="${movie.title}"
+    />
+    <div class="movie-info">
+    <h3>${movie.title}</h3>
+    <span class="${
+      movie.vote_average >= 8
+        ? "green"
+        : movie.vote_average >= 5
+        ? "orange"
+        : "red"
+    }">${movie.vote_average}</span>
+    </div>
+    <div class="overview">
+    <h3>Overview:</h3>
+    ${movie.overview}
+    </div>
+    `;
+    mainEl.appendChild(movieCardEl);
   });
 }
+
+// function getClassColor(vote) {
+//   if (vote >= 8) {
+//     return "green";
+//   } else if (vote >= 5) {
+//     return "orange";
+//   } else {
+//     return "red";
+//   }
+// }
+
+// formEl.addEventListener("submit", (event) => {
+//   event.preventDefault();
+
+//   if (searchEl.value) {
+//     getMovies(SEARCHAPI + searchEl.value);
+//   }
+
+//   searchEl.value = "";
+// });
+
+searchEl.addEventListener("keyup", () => {
+  getMovies(SEARCHAPI + searchEl.value);
+});
